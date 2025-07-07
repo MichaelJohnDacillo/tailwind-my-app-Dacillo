d03e0a12f10c8440a917f43fb8e29b40c8d4985e
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2025-07-07 15:31:42 +0800

    first commit

---
+++ dist/js/toggleMenu.js
@@ -0,0 +1,7 @@
+const headerNav = document.querySelector(".headerNav ul");
+const toggleMenu = document.querySelector(".toggleMenu");
+
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("open");
+    headerNav.classList.toggle("open");
+});
